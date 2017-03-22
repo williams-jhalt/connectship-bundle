7dad3319ec0e0b998983108a1e0826bfafdbf377
John Halterman <johnh@williams-trading.com>
2017-03-22 16:50:52 -0400

    added getting full shipment information by ucc or tracking number

--- Service/ConnectshipService.php
+++ Service/ConnectshipService.php
@@ -3,6 +3,7 @@
 namespace Williams\ConnectshipBundle\Service;
 
 use Williams\ConnectshipBundle\AMP\AMPServices;
+use Williams\ConnectshipBundle\AMP\DataDictionary;
 use Williams\ConnectshipBundle\AMP\ListCarriersRequest;
 use Williams\ConnectshipBundle\AMP\SearchRequest;
 
@@ -14,6 +15,11 @@ class ConnectshipService {
         $this->client = new AMPServices(array('soap_version' => SOAP_1_2), $wsdl_url);
     }
 
+    /**
+     * 
+     * @param string $ucc
+     * @return string
+     */
     public function getTrackingNumberByUcc($ucc) {
         
         $service = $this->client;
@@ -28,6 +34,46 @@ class ConnectshipService {
             if ($item !== null && $item[0]->getResultData()->getTrackingNumber() != null) {
                 return $item[0]->getResultData()->getTrackingNumber();
             }
+        }
+        
+    }
+    
+    /**
+     * 
+     * @param string $ucc
+     * @return DataDictionary[]
+     */
+    public function getShippingDataByUcc($ucc) {
+        
+        $service = $this->client;
+        
+        $carriersResponse = $service->ListCarriers(new ListCarriersRequest(null, null, null, null));
+
+        foreach ($carriersResponse->getResult()->getResultData()->getItem() as $carrier) {
+            $searchRequest = new SearchRequest($carrier->getSymbol(), null, null, null, null, null, null);
+            $searchRequest->setFilters(array('consigneeReference' => $ucc));
+            $searchResponse = $service->Search($searchRequest);
+            return $searchResponse->getResult()->getResultData()->getItem();
+        }
+        
+    }
+    
+    /**
+     * 
+     * @param string $trackingNumber
+     * @return DataDictionary[]
+     */
+    public function getShippingDataByTrackingNumber($trackingNumber) {
+        
+        $service = $this->client;
+        
+        $carriersResponse = $service->ListCarriers(new ListCarriersRequest(null, null, null, null));
+
+        foreach ($carriersResponse->getResult()->getResultData()->getItem() as $carrier) {
+            $searchRequest = new SearchRequest($carrier->getSymbol(), null, null, null, null, null, null);
+            $searchRequest->setFilters(array('trackingNumber' => $trackingNumber));
+            $searchResponse = $service->Search($searchRequest);
+            return $searchResponse->getResult()->getResultData()->getItem();
         }
         
     }
